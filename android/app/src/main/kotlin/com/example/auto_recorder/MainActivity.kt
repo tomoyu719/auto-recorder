f5103d926ecfd42110df3ecf6f366435577f9211
tomoyu <tomoyuki719245@gmail.com>
2024-05-23 16:36:47 +0900

    add readme images

--- android/app/src/main/kotlin/com/example/auto_recorder/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.auto_recorder
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/constants/decibel.dart
+++
@@ -1,5 +0,0 @@
-/// The maximum decibel value.
-const maxDb = 0.0;
-
-/// The minimum decibel value.
-const minDb = -100.0;
