12f4f501b5c1b36d02c4118d237b2766297e2f81
tomoyu <tomoyuki719245@gmail.com>
2024-05-28 15:29:50 +0900

    clean: Separate the Recorder class from the soundMonitor class

---
+++ lib/features/recorder.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'recorder.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$recorderHash() => r'ca1cfb391cfed066154595a9e2ac6b26dd5a8201';
+
+/// See also [recorder].
+@ProviderFor(recorder)
+final recorderProvider = Provider<Recorder>.internal(
+  recorder,
+  name: r'recorderProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$recorderHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef RecorderRef = ProviderRef<Recorder>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
