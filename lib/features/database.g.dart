6c029413009471ff95067643d9a97400aa743096
tomoyu <tomoyuki719245@gmail.com>
2024-05-28 15:28:00 +0900

    update: replace manual provider with auto generated provider

---
+++ lib/features/database.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'database.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$databaseHash() => r'aab92184c43dc681c0c8f2615262233503316fd7';
+
+/// See also [database].
+@ProviderFor(database)
+final databaseProvider = Provider<Database>.internal(
+  database,
+  name: r'databaseProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$databaseHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef DatabaseRef = ProviderRef<Database>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
